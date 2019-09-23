a2d1360f276fef2845ed80aa2c8e3628e1ee2f1b
Nikolas Railan Modesto Pereira <nikolasrailanmp@gmail.com>
2019-09-23 14:50:34 -0300

    123

--- rsc/sample/model/CategoriaDAOimpl.java
+++ rsc/sample/model/CategoriaDAOimpl.java
@@ -1,13 +1,25 @@
 package sample.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class CategoriaDAOimpl implements CategoriasDAO {
 
+    private static  String INSERT = "INSERT INTO categorias(nome) VALUES(?)";
 
     @Override
     public void insere(Categorias c) throws SQLException {
+
+        Connection con = FabricaConexao.getConnection();
+
+        PreparedStatement stm = con.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+
+        stm.setString(1,c.getNome());
+        stm.executeUpdate();
+
 
     }
 
